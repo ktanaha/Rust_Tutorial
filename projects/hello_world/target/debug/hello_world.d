6d457ac968370e951aa1739285aefcc9c2e9d346
ktanaha <moutoku1984@gmail.com>
2018-02-12 21:32:59 +0900

    first commit

---
+++ projects/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/user/Develop/rust/projects/hello_world/target/debug/hello_world.dSYM: /Users/user/Develop/rust/projects/hello_world/src/main.rs
---
+++ projects/hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/Users/user/Develop/rust/projects/hello_world/target/release/hello_world.dSYM: /Users/user/Develop/rust/projects/hello_world/src/main.rs
